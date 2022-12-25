2060e40132d462956e287445112c0a3bc10cc155
gogo199432 <9107561+gogo199432@users.noreply.github.com>
2022-12-25 22:31:16 +0100

    small transitions

--- frontend/src/routes/entrylist/[rootEntry]/+page.svelte
+++ frontend/src/routes/entrylist/[rootEntry]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import Entry from "$lib/Entry.svelte"
-    import { page } from '$app/stores';
+    import {page} from '$app/stores';
     import {onMount} from "svelte";
+    import {fade, fly} from 'svelte/transition';
+
 
     let entries : string[] = []
     onMount(function(){
@@ -14,13 +16,14 @@
     }
 
     function backHandler(event){
-        const border = entries.indexOf(event.detail.choice)
-        entries.length = border
+        entries.length = entries.indexOf(event.detail.choice)
     }
 </script>
 
 {#each entries as entry}
-    <Entry entryId={entry} on:chosen={choiceHandler} on:back={backHandler}></Entry>
-    <hr>
-    <hr>
+    <div in:fly="{{ y: -50, duration: 1000 }}" out:fade>
+        <Entry entryId={entry} on:chosen={choiceHandler} on:back={backHandler} ></Entry>
+        <hr>
+        <hr>
+    </div>
 {/each}
